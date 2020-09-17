26e3954b5a72e1a47f47d527012c20d79693629f
thisWandiPratama <wandi99pratama@gmail.com>
2020-09-17 20:39:54 +0700

    hello word 1

---
+++ hello_word/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Hello Word"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                height: 30,
+                width: 150,
+                child: Text(
+                  "Belajar Flutter Semoga Istiqomah ya",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize:  12, fontWeight: FontWeight.bold),
+                ))),
+      ),
+    );
+  }
+}
